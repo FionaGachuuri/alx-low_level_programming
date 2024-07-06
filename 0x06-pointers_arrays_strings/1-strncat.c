2ee62457c3a579e00c7ecad03ea9d02c4eb3b97d
FionaGachuuri <fionairuka@gmail.com>
2024-07-05 23:15:07 -0700

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,19 +10,20 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
-	int j = 0;
+	int i,j;
 
-if (dest[i] != '\0')
-{
-	i++;
-}
-if (j < n && src[j] != '\0')
-{
-	dest[i] = src[j];
-	i++;
-	j++;
-}
-dest[i] = '\0';
-return (dest);
-}
+	i = 0;
+	while (dest [i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0'
+		return (dest);
