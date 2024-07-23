47590819894155302e34aab11b72641e19b96f41
FionaGachuuri <fionairuka@gmail.com>
2024-07-23 21:21:29 +0300

    Task 0 defining a new type struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+  *struct dog - description of a dog
+  *@name: name of the dog
+  *@age: age of the dog
+  *@owner: owner of the dog
+  */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
+
+#endif /* _DOG_H */
