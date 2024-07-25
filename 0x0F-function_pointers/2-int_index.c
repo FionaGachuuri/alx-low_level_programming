a89e0852e898c3d04c4199f9bd3361f76a24900b
FionaGachuuri <fionairuka@gmail.com>
2024-07-26 00:09:16 +0300

    a program that performs simple operations.

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,22 +14,20 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i = 0;
 
 	if (array == NULL || cmp == NULL || size <= 0)
 	{
 		return (-1);
 	}
+	while (i < size)
 
-	for (i = 0; i < size; i++)
 	{
-		cmp(array[i]);
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+		i++;
 	}
-
-	if (cmp(array[i]) != 0)
-	{
-		return (i);
-	}
-
 	return (-1);
 }
