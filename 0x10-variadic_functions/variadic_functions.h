bf2a171c60f4dabcbe527cc7fdc4bee2a3af30d3
FionaGachuuri <fionairuka@gmail.com>
2024-07-27 00:24:56 +0300

    file holding prototypes

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,7 @@
 #include <stdarg.h>
 #include <string.h>
 
+void print_numbers(const char *separator, const unsigned int n, ...);
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 
